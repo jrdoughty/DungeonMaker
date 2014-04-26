4d96cd37fd31cc81f005d269395dbf0b3dfb6659
jrdoughty <jrdoughty13@gmail.com>
2014-04-25 22:52:29 -0500

    Adding Actor Class

---
+++ BeyondYonder/src/Base Classes/Actor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeyondYonder.src.Base_Classes
+{
+    class Actor
+    {
+        protected string name;
+        protected string race;
+        protected int size;
+
+        //protected int strengthScore;
+        //protected int dexterityScore;
+        //protected int constitutionScore;
+        //protected int intellegenceScore;
+        //protected int wisdomScore;
+        //protected int charismaScore;
+
+        //protected int strengthScoreAdj;
+        //protected int dexterityScoreAdj;
+        //protected int constitutionScoreAdj;
+        //protected int intellegenceScoreAdj;
+        //protected int wisdomScoreAdj;
+        //protected int charismaScoreAdj;
+
+        protected int movementSpeed;
+        protected bool prone;
+        
+        public string GetName()
+        {
+            return name;
+        }
+
+        public string GetRace()
+        {
+            return race;
+        }
+
+        public int GetSize()
+        {
+            return size;
+        }
+
+        public int GetMovementSpeed()
+        {
+            return movementSpeed;
+        }
+
+        public bool IsPlayerProne()
+        {
+            return prone;
+        }
+
+    }
+}
--- BeyondYonder/src/GameComponents/PC.cs
+++ BeyondYonder/src/GameComponents/PC.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BeyondYonder.src.Base_Classes;
 
-namespace BeyondYonder.Game_Screens
+namespace BeyondYonder.src.GameComponents
 {
-    class OptionsScreen
+    
+    class PC : Actor
     {
+
     }
 }
