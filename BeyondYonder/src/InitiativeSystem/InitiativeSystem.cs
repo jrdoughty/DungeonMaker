97c555de595c7d4f04c8b1886cba8fb85b959a58
Jake Watson <jake.watson76@gmail.com>
2014-04-25 23:55:10 -0500

    Initiative system

---
+++ BeyondYonder/src/InitiativeSystem/InitiativeSystem.cs
@@ -0,0 +1,69 @@
+﻿using BeyondYonder.src.Base_Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeyondYonder.src.InitiativeSystem
+{
+    public class InitiativeSystem
+    {
+        private static InitiativeSystem _instance = null;
+
+        private List<Actor> _actors;
+        public List<Actor> Actors { get; }
+
+        public static InitiativeSystem Instance()
+        {
+            if (_instance == null)
+            {
+                _instance = new InitiativeSystem();
+            }
+            return _instance;
+        }
+
+        private InitiativeSystem() { _actors = new List<Actor>(); }
+
+        public void AddActor(Actor ao)
+        {
+            if (Actors.Count == 0)
+            {
+                Actors.Add(ao);
+                return;
+            }
+
+            int index = 0;
+            foreach (Actor a in Actors)
+            {
+                if (a.Initiative >= ao.Initiative)
+                {
+                    index++;
+                }
+            }
+
+            if (index == Actors.Count)
+            {
+                Actors.Add(ao);
+            }
+            else
+            {
+                Actors.Insert(index, ao);
+            }
+        }
+
+        public void MoveActor(Actor ao, int index)
+        {
+            Actors.Remove(ao);
+            
+            if (index >= Actors.Count)
+            {
+                Actors.Add(ao);
+            }
+            else
+            {
+                Actors.Insert(index, ao);
+            }
+        }
+    }
+}
