49304495213a57d311a0ca93e4f6e60376d8302f
Jake Watson <jake.watson76@gmail.com>
2014-04-26 11:31:50 -0500

    Actor changes

--- BeyondYonder/src/Base Classes/Actor.cs
+++ BeyondYonder/src/Base Classes/Actor.cs
@@ -8,10 +8,6 @@ namespace BeyondYonder.src.Base_Classes
 {
     abstract class Actor
     {
-        protected string name;
-        protected string race;
-        protected int size;
-
         protected int strengthScore = 10;
         protected int dexterityScore = 10;
         protected int constitutionScore = 10;
@@ -26,9 +22,8 @@ namespace BeyondYonder.src.Base_Classes
         protected int wisdomScoreAdj = 0;
         protected int charismaScoreAdj = 0;
 
-        protected int movementSpeed;
         protected int initiativeBonus = 0;
-        protected bool prone;
+        protected int dieRoll = 0;
         private Random random = new Random();
 
         protected Guid characterID;
@@ -38,35 +33,12 @@ namespace BeyondYonder.src.Base_Classes
             get { return characterID; }
         }
 
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
-        }
-
-        public string Race
-        {
-            get { return race; }
-            set { race = value; }
-        }
-
-        public int Size
-        {
-            get { return size; }
-            set { size = value; }
-        }
-
-        public int MovementSpeed
-        {
-            get { return movementSpeed; }
-            set { movementSpeed = value; }
-        }
-
-        public bool Prone
-        {
-            get { return prone; }
-            set { prone = value; }
-        }
+        public string Name { get; set; }
+        public string Race { get; set; }
+        public int Size { get; set; }
+        public int MovementSpeed { get; set; }
+        public bool Prone { get; set; }
+        public int Initiative { get; set; }
 
         public int DexMod
         {
@@ -79,9 +51,10 @@ namespace BeyondYonder.src.Base_Classes
             set { initiativeBonus = value; }
         }
 
-        public int DieRoll(int dieType)
+        public int TotalInitiative
         {
-            return random.Next(0, dieType);
+            get { return InitiativeBonus + Initiative; }
         }
+
     }
 }
